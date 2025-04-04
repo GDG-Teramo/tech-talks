42a0f3580262c3c99800349d44bdda80dec93a15
Federico Pomponii <federico.pomponii@gmail.com>
2025-04-04 10:32:00 +0200

    clean project

--- src/modules/objects/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance, FastifyRequest } from "fastify";
-
-export default async function (fastify: FastifyInstance)  {
-    fastify.get('/get', async(req: FastifyRequest) => {
-        return req.user;
-    })
-} 
--- src/modules/objects/strategy/objects.strategy.ts
+++
@@ -1,7 +0,0 @@
-export interface ObjectsStrategy {
-    getObjects: () => Promise<any>;
-}
-
-export class FakeObjectsStrategy implements ObjectsStrategy {
-    getObjects = async  () => ["1","2","2"]
-}
